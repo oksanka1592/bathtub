1995088493a79478e2bce177aefc1af33fb015f4
oksanka1592 <ksuxa15lz@gmail.com>
2023-03-15 01:21:57 +0300

    index added

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,27 +1,49 @@
+// const inputName = document.querySelector('.form-request__input.name');
+// const btn = document.querySelector('.form-request__btn');
+// const phone = document.querySelector('.form-request__input.phone');
+
+// inputName.addEventListener('input', changeHandler);
+// phone.addEventListener('input', changeHandler);
+
+// function changeHandler() {
+//   if (inputName.value && phonenumber(phone)) {
+//     btn.disabled = false;
+//   } else btn.disabled = true;
+// }
+
+// function phonenumber(inputtxt) {
+//   const phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{2})[-. ]?([0-9]{2})$/;
+//   if (inputtxt.value.match(phoneno)) {
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+// }
+
 const inputName = document.querySelector('.form-request__input.name');
+const phone = document.querySelector('.form-request__input.phone')
 const btn = document.querySelector('.form-request__btn');
-const phone = document.querySelector('.form-request__input.phone');
 
-inputName.addEventListener('input', changeHandler);
-phone.addEventListener('input', changeHandler);
+inputName.addEventListener('input', handleInput);
+phone.addEventListener('input', handleInput)
 
-function changeHandler() {
-  if (inputName.value && phonenumber(phone)) {
+function handleInput(e) {
+  if (e.target === phone) e.target.value = phoneMask(e.target.value)
+  if (e.target === inputName) e.target.value = e.target.value.replace(/[^(\p{L}|\d)]/gu, '')
+
+  if (inputName.value.length >= 3 && phone.value.length === 13) {
     btn.disabled = false;
   } else btn.disabled = true;
 }
 
-function phonenumber(inputtxt) {
-  const phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{2})[-. ]?([0-9]{2})$/;
-  if (inputtxt.value.match(phoneno)) {
-    return true;
-  }
-  else {
-    return false;
-  }
+function phoneMask(phone) {
+  return phone.replace(/\D/g, '')
+    .replace(/^(\d{3})(\d)/, '$1 $2')
+    .replace(/(\d{3})(\d)/, '$1-$2')
+    .replace(/(-\d{2})(\d)/, '$1-$2')
+    .replace(/(-\d{2}-\d{2})\d+?$/, '$1');
 }
-
-
 
 $(function(){
   $('.services__btn').on('click', function () {
