e3eee88e054810b459e49d056e78f4e632f09850
oksanka1592 <ksuxa15lz@gmail.com>
2023-03-22 01:29:35 +0300

    added adaptiv

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,109 +1,3 @@
-// if (window.location.pathname === '/index.html') {
-//   const inputName = document.querySelector('.form-request__input');
-//   const phone = document.querySelector('.form-request__input.phone');
-//   const btn = document.querySelector('.form-request__btn');
-
-//   inputName.addEventListener('input', handleInput);
-//   phone.addEventListener('input', handleInput);
-
-//   const btn2 = document.querySelector('.popup__btn');
-//   const phone2 = document.querySelector('.popup__input');
-//   phone2.addEventListener('input', (e) => {
-//     e.target.value = `+7 ${phoneMask(e.target.value.slice(2))}`
-//     btn2.disabled = e.target.value.length !== 16 
-//   });
-
-//   function handleInput(e) {
-//     if (e.target === phone) e.target.value = `+7 ${phoneMask(e.target.value.slice(2))}`
-//     if (e.target === inputName) e.target.value = nameMask(e.target.value)
-
-//     if (inputName.value.length >= 3 && phone.value.length === 16) {
-//       btn.disabled = false;
-//     } else btn.disabled = true;
-//   }
-// }
-
-// if (window.location.pathname === '/services.html') {
-//   //тут код для странички сервисов, но это все дикие костыли, нужно или дальше разбираться как в одном файле делать или разбираться с галпом
-//   const btn3 = document.querySelector('.consultation__btn');
-//   const phone3 = document.querySelector('.consultation__input');
-//   phone3.addEventListener('input', (e) => {
-//     e.target.value = `+7 ${phoneMask(e.target.value.slice(2))}`
-//     btn3.disabled = e.target.value.length !== 16
-//   });
-
-//   function handleInput(e) {
-//     if (e.target === phone) e.target.value = `+7 ${phoneMask(e.target.value.slice(2))}`
-//     if (e.target === inputName) e.target.value = nameMask(e.target.value)
-
-//     if (inputName.value.length >= 3 && phone.value.length === 16) {
-//       btn.disabled = false;
-//     } else btn.disabled = true;
-//   }
-
-// }
-
-// if (window.location.pathname === '/price.html') {
-//   //тут код для странички сервисов, но это все дикие костыли, нужно или дальше разбираться как в одном файле делать или разбираться с галпом
-//   const btn3 = document.querySelector('.consultation__btn');
-//   const phone3 = document.querySelector('.consultation__input');
-//   phone3.addEventListener('input', (e) => {
-//     e.target.value = `+7 ${phoneMask(e.target.value.slice(2))}`
-//     btn3.disabled = e.target.value.length !== 16
-//   });
-
-//   function handleInput(e) {
-//     if (e.target === phone) e.target.value = `+7 ${phoneMask(e.target.value.slice(2))}`
-//     if (e.target === inputName) e.target.value = nameMask(e.target.value)
-
-//     if (inputName.value.length >= 3 && phone.value.length === 16) {
-//       btn.disabled = false;
-//     } else btn.disabled = true;
-//   }
-
-// }
-
-// function phoneMask(phone) {
-//   return phone.replace(/\D/g, '')
-//     .replace(/^(\d{3})(\d)/, '$1 $2')
-//     .replace(/(\d{3})(\d)/, '$1-$2')
-//     .replace(/(-\d{2})(\d)/, '$1-$2')
-//     .replace(/(-\d{2}-\d{2})\d+?$/, '$1');
-// }
-
-// function nameMask(name) {
-//   return name.replace(/[^(\p{L}|\d)]/gu, '')
-// }
-
-
-
-
-
-function toggleButton() {
-  var username = document.getElementById('username').value;
-  var tel = document.getElementById('tel').value;
-  
-  if (username && tel) {
-    document.getElementById('submitButton').disabled = false;
-  } else {
-    document.getElementById('submitButton').disabled = true;
-  }
-}
-
-
-function toggleButton() {
-  var phone = document.querySelector('.phone').value;
-
-  if (phone) {
-    document.querySelector('.button').disabled = false;
-  } else {
-    document.querySelector('.button').disabled = true;
-  }
-
-}
-
-
-
 $(function () {
   $('.services__btn').on('click', function () {
     $('.services__items-max').addClass('services__items-max--active');
@@ -122,19 +16,62 @@ $(function () {
     $('.header__burger,.header__menu').toggleClass('active');
     $('body').toggleClass('lock');
   });
-
-  $('.phone').mask('+7(999)999-9999');
-
-
-
-  // function toggleButton() {
-  //   var username = $('.phone').val();
-
-  //   if (username) {
-  //     $('.button').attr('disabled', false);
-  //   } else {
-  //     $('.button').attr('disabled', true);
-  //   }
-  // };
 });
+
+
+
+if (document.querySelector('.checkout__form.form-request')) {
+  formValidate('.checkout__form.form-request')
+}
+if (document.querySelector('.popup__form')) {
+  formValidate('.popup__form')
+}
+
+
+// if (document.location.pathname === '/services.html'
+//   || document.location.pathname === '/price.html') {
+
+if (document.querySelector('.consultation__form')) {
+  formValidate('.consultation__form')
+}
+
+function formValidate(selector) {
+  const form = document.querySelector(selector);
+  const inputName = form.querySelector('.name');
+  const phone = form.querySelector('.phone');
+  const btn = form.querySelector('.button');
+
+  form.addEventListener('input', handleInput);
+
+  function handleInput(e) {
+    if (e.target === phone) {
+      e.target.value = phoneMask(e.target.value)
+    }
+
+    if (inputName) {
+      if (e.target === inputName) e.target.value = nameMask(e.target.value)
+      inputName.value.length >= 3 && phone.value.length === 16 ? btn.disabled = false : btn.disabled = true
+      return
+    }
+
+    btn.disabled = phone.value.length !== 16
+  }
+}
+
+function phoneMask(phone) {
+  if (phone.slice(0, 3) === '+7 ') phone = phone.slice(3)
+  if (phone.slice(0, 2) === '+7') phone = phone.slice(2)
+  let res = phone
+    .replace(/\D/g, '')
+    .replace(/^(\d{3})(\d)/, '$1 $2')
+    .replace(/(\d{3})(\d)/, '$1-$2')
+    .replace(/(-\d{2})(\d)/, '$1-$2')
+    .replace(/(-\d{2}-\d{2})\d+?$/, '$1')
+  return '+7 ' + res
+}
+
+function nameMask(name) {
+  return name.replace(/[^(\p{L}|\d)]/gu, '')
+}
+
 
